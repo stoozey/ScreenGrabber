a87a25dd46eb30ea60ee2631ed863950cf4b4850
stoozey <stoozey123@gmail.com>
2020-11-07 18:06:51 +0000

    added post_call C# source code

---
+++ cs/post_call/post_call/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using System.Windows;
+using System.Windows.Media.Imaging;
+
+namespace post_call
+{
+	internal static class Program
+	{
+		[STAThread]
+		public static void Main(string[] _args)
+		{
+			switch (_args[0])
+			{
+				case "copy_to_clipboard":
+				{
+					Console.WriteLine("Trying to copy to clipboard...");
+					
+					var filename = _args[1];
+					if (!File.Exists(filename))
+					{
+						Console.WriteLine("File does not exist!");
+						return;
+					}
+					
+					BitmapSource bSource = new BitmapImage(new Uri(filename));
+					Clipboard.SetImage(bSource);
+					
+					Console.WriteLine("Success!");
+				} return;
+			}
+			
+			Console.WriteLine("Invalid arguments were given.");
+		}
+	}
+}
