0c7aff6dd6b9833994e20fa15623a0c104a60158
stoozey <stoozey123@gmail.com>
2020-11-07 18:57:38 +0000

    updated packages

---
+++ cs/post_call/post_call/Interfaces/IConfig.cs
@@ -0,0 +1,8 @@
+﻿namespace post_call.Interfaces
+{
+	public interface IConfig
+	{
+		string api_imgur_client_id { get; }
+		//string api_imgur_client_secret { get; }
+	}
+}
--- cs/post_call/post_call/Program.cs
+++ cs/post_call/post_call/Program.cs
@@ -2,14 +2,22 @@
 using System.IO;
 using System.Windows;
 using System.Windows.Media.Imaging;
+using Config.Net;
+using post_call.Interfaces;
 
 namespace post_call
 {
 	internal static class Program
 	{
+		
+		
 		[STAThread]
 		public static void Main(string[] _args)
 		{
+			var config = new ConfigurationBuilder<IConfig>()
+				.UseIniFile($@"{Directory.GetCurrentDirectory()}\config.json")
+				.Build();
+			
 			switch (_args[0])
 			{
 				case "copy_to_clipboard":
